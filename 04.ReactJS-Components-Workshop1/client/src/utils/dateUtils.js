78bc8391963730b9ab044045a77812bbcea63c1c
Kaloyan Nedelkov <71516863+kokolino1015@users.noreply.github.com>
2024-08-07 19:03:11 +0300

    Delete 04.ReactJS-Components-Workshop1 directory

--- 04.ReactJS-Components-Workshop1/client/src/utils/dateUtils.js
+++
@@ -1,9 +0,0 @@
-export const formatDate = (dateString) => {
-    const date = new Date(dateString);
-
-    const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
-    
-    const formattedDate = date.toLocaleDateString('en-US', dateOptions);
-
-    return formattedDate;
-};
